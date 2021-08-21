46480495a11dd68f08fa36e434c0685358e404d5
kapeing <kape.dynasty@gmail.com>
2021-08-21 22:56:27 +0900

    ガワだけ整えた

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,28 @@
       </ul>
     </nav>
     <main>
-      <div><span>100,000</span>P-BANK</div>
-      <div>貯金箱から取り出したい金額を入力するフォーム</div>
+      <ul class="bank_area">
+        <li class="bank01">
+          <span class="graph"></span>
+          <div class="bank_box"><span>100,000</span>P-BANK</div>
+        </li>
+        <li class="bank02">
+          <span class="graph"></span>
+          <div class="bank_box"><span>100,000</span>P-BANK</div>
+        </li>
+        <li class="bank03">
+          <span class="graph"></span>
+          <div class="bank_box"><span>100,000</span>P-BANK</div>
+        </li>
+      </ul>
+      <div class="withdrawal">
+        <ul class="which_bank">
+          <li>bank01</li>
+          <li>bank02</li>
+          <li>bank03</li>
+        </ul>
+        <input type="text" placeholder="出金">
+      </div>
     </main>
     <aside><div>直近の履歴（10件くらい）</div></aside>
   </section>
@@ -31,9 +51,109 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+html {
+  height: 100%;
+  body {
+    height: 100%;
+  }
+}
+section {
+  height: 100%;
+  background: #fafafa;
+  width: 1080px;
+  margin: 0 auto;
+}
 nav {
-  li {
+  ul {
     display: flex;
+    justify-content: space-around;
+    padding: 10px 0 10px;
+  }
+}
+.bank_area {
+  width: 800px;
+  height: 300px;
+  margin: 0 auto;
+  padding-top: 20px;
+  border-radius: 50px;
+  background-color: #00552e;
+  background-image:
+    linear-gradient(rgba(255,255,255,.5) 2px, transparent 2px),
+    linear-gradient(90deg, rgba(255,255,255,.5) 2px, transparent 2px),
+    linear-gradient(rgba(255,255,255,.28) 1px, transparent 1px),
+    linear-gradient(90deg, rgba(255,255,255,.28) 1px, transparent 1px);
+  background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
+  background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+  display: flex;
+  justify-content: space-around;
+  li[class*="bank0"] {
+    position: relative;
+    width: calc(180px + 10px);
+    height: calc(250px + 10px);
+    margin: 0 auto;
+    .bank_box {
+      width: 100%;
+      height: 100%;
+      z-index: 3;
+      backdrop-filter: blur(2px);
+      -webkit-backdrop-filter: blur(2px);
+      background: rgba(255,255,255,0.4);
+      border-radius: 10px;
+      border: 1px solid rgba(255,255,255,0.2);
+      position: relative;
+      display: block;
+      margin: 0 auto;
+      color: #fff;
+      text-align: center;
+      padding-top: 200px;
+    }
+    .graph {
+      width: 180px;
+      display: block;
+      position: absolute;
+      bottom: 5px;
+      left: 5px;
+      z-index: 2;
+    }
+  }
+  .bank01 .graph {
+    background-color: #fce043;
+    background-image: linear-gradient(315deg, #fce043 0%, #fb7ba2 74%);
+    height: 200px;
+  }
+  .bank02 .graph {
+    background-color: #abe9cd;
+    background-image: linear-gradient(315deg, #abe9cd 0%, #3eadcf 74%);
+    height: 50px;
+  }
+  .bank03 .graph {
+    background-color: #f2cf07;
+    background-image: linear-gradient(315deg, #f2cf07 0%, #55d284 74%);
+    height: 140px;
+  }
+}
+.withdrawal {
+  width: 600px;
+  margin: 0 auto;
+  .which_bank {
+    display: flex;
+    li {
+      margin: 10px;
+      padding: 0 8px 0;
+      line-height: 28px;
+      height: 28px;
+      background: #f0f0f0;
+      border: 1px solid #ddd;
+      border-radius: 15px;
+    }
+  }
+  input {
+    width: 600px; /*親要素いっぱい広げる*/
+    padding: 10px 15px; /*ボックスを大きくする*/
+    font-size: 16px;
+    border-radius: 3px; /*ボックス角の丸み*/
+    border: 2px solid #ddd; /*枠線*/
+    box-sizing: border-box; /*横幅の解釈をpadding, borderまでとする*/
   }
 }
 </style>
